75703e626baba2c62c7ebf32fc0aecf42c69779b
Evan <ehutch7@gmail.com>
2025-03-29 23:33:38 +0000

    Adding main go file with some basic tcp logic

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379")
+		os.Exit(1)
+	}
+
+	fmt.Println("Listening on port: 6379")
+
+	conn, err := l.Accept()
+	for {
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		conn.Write([]byte("+PONG\r\n"))
+	}
+}
